ebc9b714793be047688c55951a442b40b04e55f6
huda <motivasi.muda27@gmail.com>
2026-01-04 00:30:59 +0700

    penambahan lembur lintas hari

--- lemburan.py
+++ lemburan.py
@@ -13,37 +13,42 @@ def catat_lemburan():
             print("nama tidak boleh kosong")
             return
 
-        tanggal = input("tanggal (format: YYYY-MM-DD) ").strip()
+        tanggal = input("tanggal (format: YYYY-MM-DD): ").strip()
 
         #validasi format tanggal
-        datetime.datetime.strptime(tanggal, "%Y-%m-%d")
 
-        jam_mulai_str = input("Jam mulai lembur (format HH:MM): ")
-        jam_selesai_str = input("Jam selesai lembur(format HH:MM): ")
+        tanggal_obj = datetime.datetime.strptime(tanggal, "%Y-%m-%d")
+
+        jam_mulai_str = input("Jam mulai lembur (format HH:MM): ").strip()
+        jam_selesai_str = input("Jam selesai lembur(format HH:MM): ").strip()
 
         #parsing jam
-        jam_mulai = datetime.datetime.strptime(jam_mulai_str, "%H:%M")
-        jam_selesai = datetime.datetime.strptime(jam_selesai_str, "%H:%M")
+        jam_mulai = datetime.datetime.strptime(f"{tanggal} {jam_mulai_str}", "%Y-%m-%d %H:%M")
+        jam_selesai = datetime.datetime.strptime(f"{tanggal} {jam_selesai_str}", "%Y-%m-%d %H:%M")
 
         #input reason
-        reason = input("Masukan reason lembur: ")
+        reason = input("Masukan reason lembur: ").strip()
+
+        if jam_selesai <= jam_mulai:
+            #tambah 1 hari ke jam selesai jika melewati tengah malam
+            jam_selesai = jam_selesai + datetime.timedelta(days=1)
+            print("jam melewati tengah malam")
       
         durasi = jam_selesai - jam_mulai
 
-        #cek durasi harus positif
-        if durasi.total_seconds() <= 0:
-            print("error: jam selesai harus lebih besar dari jam mulai")
-            return
-
         #konversi durasi ke jam
         durasi_jam = durasi.total_seconds() / 3600
+
+        #tanggal selesai
+        tanggal_selesai = jam_selesai.strftime("%Y-%m-%d")
 
 
         #simpan data
 
         with open("data_lemburan.txt", "a") as f:
-            f.write(f"Nama karyawan: {nama}, Tanggal: {tanggal}, Jam mulai lembur: {jam_mulai_str}, jam selesai lembur: {jam_selesai_str},Durasi lembur: {int(durasi_jam)} Jam {int((durasi_jam % 1) * 60)} menit, Reason: {reason}\n")
-        print(f"data lemburan untuk {nama} pada {tanggal} telah dicatat")
+            f.write(f"Nama karyawan: {nama}, Tanggal Mulai: {tanggal}, Jam mulai lembur: {jam_mulai_str}, Tanggal selesai: {tanggal_selesai}, jam selesai lembur: {jam_selesai_str},Durasi lembur: {int(durasi_jam)} Jam {int((durasi_jam % 1) * 60)} menit, Reason: {reason}\n")
+        print(f"data lemburan untuk {nama} telah dicatat")
+        print(f"tanggal mulai: {tanggal}, tanggal selesai: {tanggal_selesai}")
         print(f"durasi: ({int(durasi_jam)} jam {int((durasi_jam % 1) * 60)} menit)")
 
     except ValueError as e:
